e77fc6b480fc97681323a97927184d3f7c8f8a1c
sks006 <mdshihabkabir07@gmail.com>
2025-12-05 07:20:34 +0600

    result is  15t done

---
+++ 5_12_2025/Option_result/src/main.rs
@@ -0,0 +1,253 @@
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("this cant be the divide by 0".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let result=divide(200,3);
+//     match result{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("Error {}",err)
+//     }
+// }
+//---------------------------- 2 -------------------------
+// fn divide(a:u64,b:u64)->Result<u64,String>{
+//     if b==0{
+//         Err("cant be divide by 0".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// } 
+
+// fn main(){
+//     let l=divide(23,3);
+//     match l{
+//         Ok(value)=>println!("Value {}",value),
+//         Err(err)=>println!("Err {}",err)
+//     }
+// }
+//---------------------------- 3 -------------------------
+// fn divide (a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cant be divide by 0".to_string())
+//     }else{
+
+//         Ok(a/b)
+//     }
+   
+// }
+
+// fn main(){
+//     let l= divide(23.4,3.4);
+//     match l{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("err {}",err)
+//     }
+// }
+//---------------------------- 4 -------------------------
+
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("cant be divide by 0".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let l=divide(234,4);
+//     match l{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("err {}",err)
+//     }
+// }
+//---------------------------- 5-------------------------
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//          Err("cant be divide by 0".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let l=divide(234,3);
+//     match l{
+//                 Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("err {}",err)
+//     }
+// }
+
+//---------------------------- 6-------------------------
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("cant divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let l=divide(2342,3);
+//     match l{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("Error {}",err)
+//     }
+// }
+
+//---------------------------- 7-------------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cant be divide".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let l=divide(34.0,0.0);
+//     match l{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("Err {}",err)
+//     }
+// }
+//---------------------------- 8-------------------------
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("this is the end".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let l=divide(2323,22);
+//     match l{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("value {}",err)
+//     }
+
+// }
+
+//---------------------------- 9  -------------------------
+
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("this is the end".to_string())
+//     }
+//     else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let l=divide(324,2);
+//     match l{
+//                 Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("value {}",err)
+//     }
+// }
+
+//---------------------------- 10  -------------------------
+
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("this is the end".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let l=divide(34.3,2.3);
+//     match l{
+//                         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("value {}",err)
+//     }
+// }
+
+//---------------------------- 11  -------------------------
+
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("the end".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let l=divide(233,3);
+//         match l{
+//                         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("value {}",err)
+//     }
+// }
+
+//---------------------------- 12  -------------------------
+
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("this is the end".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let l=divide(24.4,3.3);
+//     match  l{
+//         Err(err)=>println!("err {}",err),
+//         Ok(value)=>println!("value {}",value),
+//     }
+
+// }
+
+//---------------------------- 13  ---------------------------
+
+// fn divide(a:u32,b:u32)->Result<u32, String>{
+//     if b==0{
+//         Err("the end".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let l=divide(234,23);
+//     match l{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("Error {}",err)
+//     }
+// }
+
+//---------------------------- 14  ---------------------------
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("this is the end".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let l=divide(234,22);
+//     match l{
+//         Ok(value)=>println!("Ok :{}",value),
+//         Err(err)=>println!("Err :{}",err)
+//     }
+// }
+//---------------------------- 15  ---------------------------
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("this is  the end".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let l=divide(233,0);
+//     match l{
+//         Ok(value)=>println!("value {}",value),
+//         Err(err)=>println!("err {}",err)
+//     }
+// }
+
