4a59b7887d313ea0275c7cc4386744275afafcee
sks006 <mdshihabkabir07@gmail.com>
2025-12-14 07:39:23 +0600

    15 ownership is  done

---
+++ 13_12_2025/struct_build/src/main.rs
@@ -0,0 +1,426 @@
+// struct Loan {
+//     id: u32,
+//     amount: f32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: f32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share as f32;
+//         return self.amount as u32
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(2,233.3,40);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 19 -------------------------
+// struct Loan {
+//     amount: f32,
+//     id: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: f32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share as f32;
+//         return self.amount as u32
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(4,33333.4,40);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 18 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: f32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: f32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share as f32;
+//         return self.amount as u32
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(4,334443.4,40);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 17 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: f32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: f32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share as f32;
+//         return self.amount as u32
+//     }
+// }
+// fn main(){
+//        let mut l=Loan::new(4,4443.4,40);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 16 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         return self.amount
+//     }
+// }
+// fn main(){
+//            let mut l=Loan::new(4,4443,40);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 15 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: f32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: f32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share as f32;
+//         return self.amount as u32
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(3,3000.0,20);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 14 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share ;
+//         self.amount
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(3,30000,20);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 13 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+
+// fn main(){
+//     let mut l=Loan::new(32,300000,40);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 12 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(3,40000,9);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 11 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+
+// fn main(){
+//     let mut l=Loan::new(2,40000,4);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 10 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+
+// fn main(){
+//     let mut l=Loan::new(3,40000,4);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 9 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(3,5999,5);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 8 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self,pro:u32)->u32{
+//         self.amount+=self.share*pro;
+//         return self.amount
+//     }
+// }
+
+// fn main(){
+//     let mut l=Loan::new(2,30000,400);
+//     let y=l.inc_amount(10);
+//     println!("value {}",y)
+// }
+//------------------------------ 7 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: f32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: f32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self,pro:u32)->u32{
+//         self.amount+=(pro as f32)*self.share as f32;
+//         self.amount as u32
+
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(2,30000.0,400);
+//     let y=l.inc_amount(10);
+//     println!("value {}",y)
+// }
+//------------------------------ 6 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: f32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: f32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share as f32;
+//         self.amount as u32
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(3,34444.6,10);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 5 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+
+// fn main(){
+//     let mut l=Loan::new(2,34444,400);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 4 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+// fn main(){
+//        let mut l=Loan::new(2,34444,400);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 3 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+
+// fn main(){
+//     let mut l=Loan::new(3,30000,3);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 2 -------------------------
+// struct Loan {
+//     id: u32,
+//     amount: u32,
+//     share: u32,
+// }
+// impl Loan {
+//     fn new(id: u32, amount: u32, share: u32)->Self{
+//         Self{
+//             id,amount,share
+//         }
+//     }fn inc_amount(&mut self)->u32{
+//         self.amount+=self.share;
+//         self.amount
+//     }
+// }
+// fn main(){
+//     let mut l=Loan::new(3,30000,3);
+//     let y=l.inc_amount();
+//     println!("value {}",y)
+// }
+//------------------------------ 1 -------------------------
+struct Loan {
+    id: u32,
+    amount: u32,
+    share: u32,
+}
+impl Loan {
+    fn new(id: u32, amount: u32, share: u32)->Self{
+        Self{
+            id,amount,share
+        }
+    }fn inc_amount(&mut self,pro:u32)->u32{
+        self.amount+=self.share*pro;
+        self.amount
+    }
+}
+fn main(){
+    let mut l=Loan::new(3,30000,3);
+    let y=l.inc_amount(3);
+    println!("value {}",y)
+}
---
+++ 14_12_2025/ownership_borrowing/src/main.rs
@@ -0,0 +1,179 @@
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x+y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("40000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 14 ---------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=3000;
+//     let z:u32=x+y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("50000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 13 ---------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=6000;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("70000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 12 ---------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=1000;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("100000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 11 ---------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=4000;
+//     let z:u32=x+y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("60000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 10 ---------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=8000;
+//     let z:u32=x+y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("70000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 9 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x*y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("100000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 8 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=1500;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("400000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 7 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x*y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("5000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 6 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=7000;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("30000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 5 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("40000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 4 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=600;
+//     let z:u32=x+y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("60000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 3 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=1000;
+//     let z:u32=x*y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("300000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 2 -----------------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=7000;
+//     let z:u32=x/y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("1000000000");
+//     let y=borrow(l);
+//     println!("value {}",y)
+// }
+//----------------------------- 1 -----------------------------
+fn borrow(a:String)->u32{
+    let x:u32=a.parse().expect("this is the end");
+    let y:u32=500;
+    let z:u32=x+y;
+    return z
+}
+fn main(){
+    let l=String::from("100000");
+    let y=borrow(l);
+    println!("value {}",y)
+}
